96a7e68e31bca72de0b9ec7c53c0df9ee6861556
Chance Stahura <69828281+Chance-Stahura@users.noreply.github.com>
2026-04-02 10:05:07 -0700

    update imports for readability and add initial CBSD68 dataset (#2)

--- autoencoder.py
+++ autoencoder.py
@@ -4,10 +4,24 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-import keras
-import tensorflow
-from keras import layers, model, optimizers
-from keras.layers import Dense, Input, Reshape, Flatten, Conv2D, Conv2DTranspose
+import tensorflow as tf
+import os
+from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras.layers import Dense, Input, Reshape, Flatten, Conv2D, Conv2DTranspose
+
+#download + extract dataset
+url = 'https://github.com/clausmichele/CBSD68-dataset/archive/refs/heads/master.zip'
+cbsd_path = tf.keras.utils.get_file(
+    'cbsd68.zip', 
+    origin = url, 
+    extract=True,
+    cache_dir = '.', # current directory
+    cache_subdir = 'data' # save to ./data/
+)
+
+#get the actual image files
+#get base dir where cbsd zip is extracted, create data sub directories for data
+cbsd_img_folder = os.path.join('data', 'cbsd68_extracted', 'CBSD68-dataset-master', 'CBSD68', 'original_png')
 
 
 # %% [markdown]
@@ -15,6 +29,11 @@ from keras.layers import Dense, Input, Reshape, Flatten, Conv2D, Conv2DTranspose
 # normalize pixel values 
 
 # %%
+    # create dataset class
+    # apply transformations
+    # add noise on the fly
+    # split data
+    # create data loaders
 
 
 # %% [markdown]
